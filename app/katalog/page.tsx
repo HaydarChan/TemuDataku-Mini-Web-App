958b25b9a2747a1a2d3467594ca8effb7f99b6fa
Atqiya Haydar <atqiyahaydar15@gmail.com>
2025-06-30 16:41:19 +0700

    feat: ini katalog page

--- app/katalog/page.tsx
+++ app/katalog/page.tsx
@@ -1,7 +1,23 @@
+'use client'
+import { useAuthStore } from '@/stores/authStore'
+import { useRouter } from 'next/navigation'
+
 export default function KatalogPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+    const router = useRouter()
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
+
+  if (!isLoggedIn) {
+    router.push("/log-in")
+  }
+
+  return (
+    <div>
+      <h1>Katalog Produk TemuDataku</h1>
+      <ul>
+        <li>TemuData Starter</li>
+        <li>TemuData Pro</li>
+        <li>TemuData Enterprise</li>
+      </ul>
+    </div>
+  )
 }
